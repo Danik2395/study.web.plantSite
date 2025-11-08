3eef757955d95a21da692c92a25e2bf9d48d1e79
Daniil Kalenkovich <daniil.kalenovich@gmail.com>
2025-11-08 18:41:41 +0300

    "Fixied" header layout

--- index.js
+++ index.js
@@ -20,16 +20,16 @@ function hiddenMenuActivator() {
     hiddenMenuHeight = getComputedStyle(hiddenMenu).height;
 
     if (!hiddenMenuOpened) {
-        hiddenMenu.style = `transition: all 0.4s cubic-bezier(0.72,-0.01,0.52,0.95) 0.3s; bottom: -${hiddenMenuHeight};`;
+        hiddenMenu.style = `--visibility-headerNav: visiable; transition: all 0.4s cubic-bezier(0.72,-0.01,0.52,0.95) 0.3s; bottom: -${hiddenMenuHeight};`;
         hiddenMenuButton.style = "transition: top 0.6s cubic-bezier(0.51,-1.35,0.74,0.05); top: -240%;";
-        hiddenMenuButton.setAttribute("aria-label","Скрыть меню")
     }
     else {
         hiddenMenu.style = "transition: all 0.6s cubic-bezier(0.40,0.00,0.52,1.03); bottom: 0;";
         hiddenMenuButton.style = "transition: top 0.6s cubic-bezier(0.40,0.00,0.52,1.03); top: -40%;";
-        hiddenMenuButton.setAttribute("aria-label","Показать меню")
+        hiddenMenu.style.setProperty("--visibility-headerNav", "hidden");
     } 
 
+    hiddenMenuButton.setAttribute("aria-label", hiddenMenuOpened ? "Показать меню" : "Скрыть меню");
     hiddenMenuOpened = !hiddenMenuOpened;
 }
 hiddenMenuButton.addEventListener('click', hiddenMenuActivator);
